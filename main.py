75f658eb0321ba8e44eaa898ea116be3b0d448db
Charlie-McLarty <cmclarty21@gmail.com>
2024-03-31 19:44:33 -0400

    separated labels and pixel data

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import pyspark
 from pyspark.sql import *
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .appName("HandwritingRecognitionModel") \
+        .master("local[*]") \
         .getOrCreate()
 
-    sc = spark.sparkContext
-    print(sc)
+    # Balanced dataset has equal amount of samples for each character
+    # First column is the label, other 784 represent each pixel for 28x28 image
+    train_df = (spark.read
+                .csv("./data/emnist-balanced-train.csv", header=False))
+    train_df = train_df.withColumnRenamed("_c0", "label")
 
-    df = (spark.read.option("header", "")
-          .csv("./data/emnist-balanced-train.csv", header=False))
-    print(df.count())
+    labels = train_df.select("label")
+    pixels = train_df.drop("label")
 
-    df.show()
