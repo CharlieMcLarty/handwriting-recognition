36615baa1500514244591bf0cea830d2dcce738e
Charlie-McLarty <cmclarty21@gmail.com>
2024-04-04 17:18:33 -0400

    converted program to notebook style and fixed matplot errors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# In[1]:
 import pyspark
 from pyspark.sql import *
 from keras.datasets import mnist
@@ -5,36 +6,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pyspark.sql.types import FloatType
+from pyspark.ml.linalg import Vectors
 
-if __name__ == "__main__":
-    spark = SparkSession.builder \
-        .appName("HandwritingRecognitionModel") \
-        .master("local[*]") \
-        .getOrCreate()
+# In[2]:
+spark = SparkSession.builder \
+    .appName("HandwritingRecognitionModel") \
+    .master("local[*]") \
+    .getOrCreate()
 
-    # Balanced dataset has equal amount of samples for each character
-    # First column is the label, other 784 represent each pixel for 28x28 image
-    train_df = (spark.read
-                .option("inferSchema", "true")
-                .csv("./data/emnist-balanced-train.csv", header=False))
-    train_df = train_df.withColumnRenamed("_c0", "label")
-    mapping = spark.read.text("./data/emnist-balanced-mapping.txt")
+# Balanced dataset has equal amount of samples for each character
+# First column is the label, other 784 represent each pixel for 28x28 image
+train_df = (spark.read
+            .option("inferSchema", "true")
+            .csv("./data/emnist-balanced-train.csv", header=False))
+train_df = train_df.withColumnRenamed("_c0", "label")
+mapping = spark.read.text("./data/emnist-balanced-mapping.txt")
 
-    # Create separate dataframe for labels and pixels
-    labels = train_df.select("label")
-    pixels = train_df.drop("label")
+# In[3]:
+# Create separate dataframe for labels and pixels
+labels = train_df.select("label")
+pixels = train_df.drop("label")
 
-    # Change column types to float from string (takes long time to run)
-    # for col in pixels.columns:
-    #     pixels = pixels.withColumn(col, pixels[col].cast(FloatType()))
+# In[4]:
+# Prints shape of labels and pixels dataframe
+print((labels.count(), len(labels.columns)))
+print((pixels.count(), len(pixels.columns)))
 
-    # pixels.printSchema()
-
-    # Prints shape of labels and pixels dataframe
-    print((labels.count(), len(labels.columns)))
-    print((pixels.count(), len(pixels.columns)))
-
-    print(pixels.dtypes)
-
-    # plt.imshow(pixels.take(1)[0])
-    # plt.show()
+# Converts first row of pixels to dictionary and extracts the values
+# to convert to a 28x28 numpy array
+first_row = pixels.take(1)[0].asDict()
+pixels_values = list(first_row.values())
+print(pixels_values)
+numpy_array = np.array(pixels_values, dtype=float).reshape(28, 28)
+#
+plt.imshow(numpy_array, cmap="gray")
+plt.show()
